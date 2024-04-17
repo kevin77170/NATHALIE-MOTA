9a855ff08a0ec2882dd8f3e8da9f74710b957a1b
kevin77170 <kevlan77@live.fr>
2024-04-17 16:46:38 +0200

    étape 2 terminé

--- themes/mota-custom-themes/assets/template-part/modal-contact.php
+++ themes/mota-custom-themes/assets/template-part/modal-contact.php
@@ -1,6 +1,6 @@
 <div id="myModal" class="modal">
   <div class="modal-content">
-    <img class="logo-header" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/logo-contact-header.png'; ?> " alt="logo contact header">
+    <img class="logo-header" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/logo-contact-header.png.'; ?> " alt="logo contact header">
     <?php echo do_shortcode('[contact-form-7 id="bbff96f" title="formulaire de contact Nathalie Mota"]'); ?>
   </div>
 </div>
