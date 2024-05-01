a662d4b9dd9d15e0741a176817d0551cac869e12
kevin77170 <kevlan77@live.fr>
2024-05-01 16:03:28 +0200

    maj

--- themes/mota-custom-themes/front-page.php
+++ themes/mota-custom-themes/front-page.php
@@ -2,4 +2,5 @@
 <div class="hero-header">
 <img src="<?php echo  get_stylesheet_directory_uri() . '/assets/img/heroheader.png'; ?> " alt="Hero header du site de Nathalie Mota">
     </div>
+    
 <?php get_footer() ?>
--- themes/mota-custom-themes/index.php
+++ themes/mota-custom-themes/index.php
@@ -3,7 +3,6 @@ get_header();
 ?>
 
 <h1>bonjour index.php</h1>
-
 <?php
 get_footer();
 ?>
