1f63aeef6b9c0fc0a8197053c16c2d70253a9d9e
kevin77170 <kevlan77@live.fr>
2024-05-28 12:26:23 +0200

    dernier commit
    dernier commit

--- themes/mota-custom-themes/page.php
+++ themes/mota-custom-themes/page.php
@@ -12,23 +12,17 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-
-
     <!-- Boucle WordPress pour afficher le contenu de la page -->
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
             <h1><?php get_the_title(); ?></h1>
             <section class="container">
                 <?php the_content(); ?>
-
-
             </section>
 
     <?php endwhile;
     endif; ?>
-
-
-
+	
 </main>
 <?php
 get_footer();
